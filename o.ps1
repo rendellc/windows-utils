4926ffeacf4f80eddae648aa5966dbe7ae64987e
Rendell Cale <rendell.cale@semcon.com>
2023-10-10 14:46:59 +0200

    Autoopen nvim for godot projects

--- o.ps1
+++ o.ps1
@@ -1,8 +1,27 @@
+# 
 $search_dir = $env:DEV_DIR
 $selected=Get-ChildItem $search_dir | fzf 
+if ([string]::IsNullOrEmpty($selected)) {
+	Write-Host "No folder selected"
+	Exit
+}
+
+
 $selected=$selected -replace '\s+', ' '
 $selected=$selected -split ' '
 $selected=$selected[3]
 
-Set-Location -Path $search_dir\$selected
+$targetLocation=$search_dir+"\"+$selected
 
+$potentialGodotFile=$targetLocation+"\project.godot"
+$targetIsGodot=Test-Path $potentialGodotFile -PathType leaf
+
+if ($targetIsGodot) {
+	Write-Output "Folder is godot project"
+}
+
+Set-Location -Path $targetLocation
+
+if ($targetIsGodot) {
+	nvim --listen \\.\pipe\nvim.godot.pipe .
+}
