a10f440d59e0f33430b974026e97f288e0d6068d
Rendell Cale <rendell.cale@semcon.com>
2024-03-01 16:13:41 +0100

    Make it search recursively

--- oc.ps1
+++ oc.ps1
@@ -1,10 +1,9 @@
 
 
-$selected=Get-ChildItem -Directory -Recurse -Name -Depth 2 | fzf 
+$selected=Get-ChildItem -Directory -Recurse -Name | fzf 
 if ([string]::IsNullOrEmpty($selected)) {
 	Write-Host "No folder selected"
 	Exit
 }
 
 Set-Location -Path $selected
-Set-TabTitle -Title $selected
